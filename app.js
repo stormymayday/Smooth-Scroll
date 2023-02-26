c33a2636c6a869540ed232dee3f069d1f3db18d9
stormymayday <stormymayday7@gmail.com>
2023-02-25 23:48:36 -0700

    Implemented nav-toggle functionality.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // Dynamic Date
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
+
+// close links
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function () {
+
+    linksContainer.classList.toggle('show-links');
+
+}); 
