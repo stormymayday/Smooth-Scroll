d114c3db3a915a92f4c8d7fa80d5255ff2d68a5f
stormymayday <stormymayday7@gmail.com>
2023-02-26 19:34:33 -0700

    Removed .links class styling as it was redundant due to sames style on .links-container.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 
-// close links
+// navToggle functionality
 const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
